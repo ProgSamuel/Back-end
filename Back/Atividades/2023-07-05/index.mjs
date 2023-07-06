3c4be93321695cbb73761568bc055f456c409e08
Samuel <progsamu@gmail.com>
2023-07-06 19:36:19 -0300

    aula 20230705

---
+++ Back/Atividades/2023-07-05/index.mjs
@@ -0,0 +1,14 @@
+import avai from "./teste.mjs";
+
+console.log("inicio do programa");
+
+/* const teste = "teste"
+
+function somar(a,b) {
+return a+b    
+}
+
+console.log(somar(1,2));
+console.log(teste); */
+
+console.log(avai);
---
+++ Back/Atividades/2023-07-05/teste.mjs
@@ -0,0 +1,11 @@
+export function somar(a,b) {
+    return a+b    
+    }
+
+export const teste = "teste"
+
+export var teste2 = "teste2"
+
+export let teste3 = "teste3"
+
+export default 5
