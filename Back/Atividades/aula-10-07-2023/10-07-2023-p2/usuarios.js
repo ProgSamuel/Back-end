d3ac41abee986181b48f21a16e3a3ff33a5fb791
Samuel <progsamu@gmail.com>
2023-07-11 19:22:12 -0300

    aula 10 de julho

---
+++ Back/Atividades/aula-10-07-2023/10-07-2023-p2/usuarios.js
@@ -0,0 +1,8 @@
+import expressUser from "./usuarios.js";
+import express, { json } from "express";
+const app = express();
+app.use(json());
+expressUser(app);
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
+});
